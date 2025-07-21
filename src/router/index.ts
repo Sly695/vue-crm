b2bf476b45758e51df49e5d65f87b86ab3a22885
Sly695 <sandaraly1@gmail.com>
2025-07-21 22:23:08 +0200

    add users and dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/LoginPage.vue'
 import Products from '@/components/Products.vue'
+import Dashboard from '@/components/Dashboard.vue'
+import Users from '@/components/users.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,12 +13,22 @@ const router = createRouter({
       component: LoginPage,
     },
     {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+    },
+    {
       path: '/products',
       name: 'products',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: Products,
+    },
+        {
+      path: '/users',
+      name: 'users',
+      component: Users,
     },
   ],
 })
